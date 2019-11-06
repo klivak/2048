5f222bc0e2eef45b5cc8656e070957d1ad5ee610
vklivak <vladklivak@gmail.com>
2019-11-06 11:01:44 +0200

    Added styles and resize event

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,31 @@
+const game;
+
 window.onload = function() {
-    let game = new Phaser.Game();
+    let gameConfig = {
+        width: 480,
+        height: 640,
+        backgroundColor: 0xff0000
+    };
+
+    game = new Phaser.Game(gameConfig);
+
+    window.focus();
+    resizeGame();
+    window.addEventListener('resize', resizeGame);
+}
+
+function resizeGame() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+    canvas.style.width = (windowHeight * gameRatio) + "px";
+    canvas.style.height = windowHeight + "px";
+    }
 }
